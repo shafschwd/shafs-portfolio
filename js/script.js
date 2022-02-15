dd46a76a8c89a5373fef27d15e0d6c03e0e9c3a8
shafs <35454710+shafschwd@users.noreply.github.com>
2022-02-16 00:48:18 +0600

    redesigned

--- js/script.js
+++ js/script.js
@@ -13,27 +13,4 @@ $(function () {
 	});
 
 	// menu fix start
-
-	// top-bottom-btn fix start
-
-	$(".btn_top_bottom").click(function () {
-		$("html").animate(
-			{
-				scrollTop: 0,
-			},
-			2000
-		);
-	});
-
-	$(window).scroll(function () {
-		var scrolling = $(this).scrollTop();
-
-		if (scrolling > 200) {
-			$(".btn_top_bottom").fadeIn();
-		} else {
-			$(".btn_top_bottom").fadeOut();
-		}
-	});
-
-	// top-bottom-btn fix end
 });
